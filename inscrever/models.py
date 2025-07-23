e6b8db19f72d27bc0c550107dad65522b05ac8fd
robinson <robinpaulista85@gmail.com>
2025-07-23 15:25:01 -0300

    Atualiza admin, models, views e configurações; remove templates antigos

--- inscrever/models.py
+++ inscrever/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Cuidador(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    experiencia = models.TextField(blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
+    estado = models.CharField(max_length=2, blank=True, null=True)
+    disponibilidade = models.TextField(blank=True, null=True)
+    
+    
+    def __str__(self):
+        return self.nome
--- inscrever/views.py
+++ inscrever/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cuidador
+from .forms import CuidadorForm
 
-# Create your views here.
+
+def lista_cuidadores(request):
+    cuidadores = Cuidador.objects.all()
+    return render(request, 'inscrever/lista_cuidadores.html', {'cuidadores': cuidadores})
+
+
+def novo_cuidador(request):
+    if request.method == 'POST':
+        form = CuidadorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cuidadores')
+    else:
+        form = CuidadorForm()
+    return render(request, 'inscrever/novo_cuidador.html', {'form': form})
