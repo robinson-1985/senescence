51b632e32eeb76070ed0e3016e5da81a4b55392d
robinson <robinpaulista85@gmail.com>
2025-07-24 19:57:54 -0300

    feat: Configura novas rotas para o app inscrever e projeto principal

--- inscrever/urls.py
+++ inscrever/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import CuidadorDetailView, ListaCuidadorView, NovoCuidadorView
 
 urlpatterns = [
-    path('', views.lista_cuidadores, name='lista_cuidadores'),
-    path('novo/', views.novo_cuidador, name='novo_cuidador'),
+    path('', ListaCuidadorView.as_view(), name='cuidadores_lista'),
+    path('novo/', NovoCuidadorView.as_view(), name='cuidadores_novo'),
+    path('<int:pk>/', CuidadorDetailView.as_view(), name='cuidadores_detalhe'),
 ]
