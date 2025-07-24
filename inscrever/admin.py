d7726c41148512d7a1ff9519e7270c19ffe65c4a
robinson <robinpaulista85@gmail.com>
2025-07-24 19:57:45 -0300

    refactor: Atualiza lógica das views e configurações do admin

--- inscrever/admin.py
+++ inscrever/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Cuidador
 
-admin.site.register(Cuidador)
+@admin.register(Cuidador)
+class CuidadorAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'cidade', 'estado', 'disponibilidade']
+    search_fields = ['nome', 'email', 'cidade']
+    list_filter = ['estado', 'disponibilidade']
--- inscrever/views.py
+++ inscrever/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cuidador
 from .forms import CuidadorForm
 
 
-def lista_cuidadores(request):
-    cuidadores = Cuidador.objects.all()
-    return render(request, 'inscrever/lista_cuidadores.html', {'cuidadores': cuidadores})
+class ListaCuidadorView(ListView):
+    model = Cuidador
+    template_name = 'inscrever/lista.html'
+    context_object_name = 'cuidadores'
+    paginate_by = 10  # Mostra 10 cuidadores por página
 
 
-def novo_cuidador(request):
-    if request.method == 'POST':
-        form = CuidadorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('lista_cuidadores')
-    else:
-        form = CuidadorForm()
-    return render(request, 'inscrever/novo_cuidador.html', {'form': form})
+class NovoCuidadorView(LoginRequiredMixin, CreateView):
+    model = Cuidador
+    form_class = CuidadorForm
+    template_name = 'inscrever/novo.html'
+    success_url = reverse_lazy('lista_cuidadores')
+    login_url = reverse_lazy('login')  # Redireciona para login se não estiver autenticado
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Cadastro realizado com sucesso!')
+        return super().form_valid(form)
+
+
+class CuidadorDetailView(DetailView):
+    model = Cuidador
+    template_name = 'inscrever/detalhe.html'
+    context_object_name = 'cuidador'
